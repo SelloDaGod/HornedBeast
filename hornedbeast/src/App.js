9042aec51d049dc5431e713d628f07ab881548e5
SelloDaGod <Marcel.mk1577@gmail.com>
2023-04-26 13:20:00 -0500

    finished lab 03

--- hornedbeast/src/App.js
+++ hornedbeast/src/App.js
@@ -2,12 +2,22 @@ import Header from './Header';
 import Footer from './Footer';
 import Main from './main';
 import './App.css';
-
+import data from './data.json';
+import { useState } from 'react';
+import Selectedbeast from './SelectedBeast';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
+  const [clickedbeast, setclickedbeast] = useState(data[0]);
+  const [displaySelectedBeast, setdisplaySelectedBeast] = useState(false)
+ 
+  // let beastSelect = () =>{
+  //   setclickedbeast(clickedbeast)
+  // }
   return (
     <div className="App">
       <Header/>
-      <Main/>
+      <Main beastdata={data} setdisplaySelectedBeast={setdisplaySelectedBeast} Selectedbeast={setclickedbeast}/>
+      <Selectedbeast clickedbeast={clickedbeast}  displaySelectedBeast={displaySelectedBeast}  setdisplaySelectedBeast={setdisplaySelectedBeast} />
       <Footer/>
     </div>
   );
--- hornedbeast/src/HornedBeast.js
+++ hornedbeast/src/HornedBeast.js
@@ -1,19 +1,23 @@
 import { useState } from "react";
+import Selectedbeast from "./SelectedBeast";
 
 
-function Hornedbeast(props){
-const [Clickcount,setClickcount] = useState(0);
-let hornClickcount = () => {
-    setClickcount(Clickcount + 1)
+function Hornedbeast(props) {
+    const [Clickcount, setClickcount] = useState(0);
+    let hornClickcount = () => {
+        setClickcount(Clickcount + 1)
+    }
+    return (
+        <div >
+            <h2>{props.title}</h2>
+            <img onClick={function(){
+                props.setdisplaySelectedBeast(true);
+                 props.Selectedbeast(props.clickedbeast)
+            }} height={200} title={props.title} alt={props.description} src={props.image}></img>
+            <p>{props.description}</p>
+            {Clickcount} <button onClick={hornClickcount} class="bi bi-bookmark-heart-fill"></button>
+            
+        </div>
+    );
 }
-return(
-    <div>
-        <h2>{props.title}</h2>
-        <img onClick={hornClickcount} height={200} title ={props.title} alt = {props.description} src = {props.image}></img>
-    
-        <p>{props.description}</p> 
-        {Clickcount} <i class="bi bi-bookmark-heart-fill"></i>
-    </div>
-);
-} 
 export default Hornedbeast;
